5814f9f1c0ea6e40fb5d9a2a7d4c34642c06e385
zhaoyun <zhaoyun1224@gmail.com>
2026-05-02 23:58:21 +0800

    refactor: BaseUseCase self-registers via requireServiceRegistry, deprecates attachParent

--- scaffold/core/src/main/kotlin/zhaoyun/example/composedemo/scaffold/core/usecase/BaseUseCase.kt
+++ scaffold/core/src/main/kotlin/zhaoyun/example/composedemo/scaffold/core/usecase/BaseUseCase.kt
@@ -9,9 +9,12 @@ import zhaoyun.example.composedemo.scaffold.core.mvi.StateHolder
 import zhaoyun.example.composedemo.scaffold.core.mvi.UiEffect
 import zhaoyun.example.composedemo.scaffold.core.mvi.UiEvent
 import zhaoyun.example.composedemo.scaffold.core.mvi.UiState
-import zhaoyun.example.composedemo.scaffold.core.spi.MutableServiceRegistryImpl
+import zhaoyun.example.composedemo.scaffold.core.spi.MutableServiceRegistry
 import zhaoyun.example.composedemo.scaffold.core.spi.ServiceRegistry
+import zhaoyun.example.composedemo.scaffold.core.spi.autoRegister
+import zhaoyun.example.composedemo.scaffold.core.spi.autoUnregister
 import zhaoyun.example.composedemo.scaffold.core.spi.find
+import zhaoyun.example.composedemo.scaffold.core.spi.requireServiceRegistry
 
 typealias UseCaseFactory<S, E, F> = (StateHolder<S>) -> BaseUseCase<S, E, F>
 
@@ -25,40 +28,50 @@ abstract class BaseUseCase<S : UiState, E : UiEvent, F : UiEffect>(
 
     abstract suspend fun onEvent(event: E)
 
-    protected val serviceRegistry = MutableServiceRegistryImpl()
-
-    fun attachParent(registry: ServiceRegistry) {
-        serviceRegistry.attachParent(registry)
+    init {
+        autoRegister(requireServiceRegistry())
     }
 
+    @Deprecated(
+        "attachParent is no longer needed. ServiceRegistry is now shared per-Screen via Koin Scope.",
+        ReplaceWith("")
+    )
+    fun attachParent(registry: ServiceRegistry) {
+        // no-op: retained for binary compatibility during migration
+    }
+
+    @Deprecated(
+        "detachParent is no longer needed.",
+        ReplaceWith("")
+    )
     fun detachParent() {
-        serviceRegistry.detachParent()
+        // no-op
     }
 
     protected inline fun <reified T : Any> findService(tag: String? = null): T {
-        return serviceRegistry.find<T>(tag)
+        return requireServiceRegistry().find<T>(tag)
             ?: error(
                 "Service ${T::class.java.name} not found in current scope. " +
-                        "Did you forget to register it from a ServiceProvider or auto-expose an MviService?"
+                    "Did you forget to register it from a ServiceProvider or auto-expose an MviService?"
             )
     }
 
     protected inline fun <reified T : Any> findServiceOrNull(tag: String? = null): T? {
-        return serviceRegistry.find<T>(tag)
+        return requireServiceRegistry().find<T>(tag)
     }
 
     protected inline fun <reified T : Any> registerService(
         instance: T,
         tag: String? = null,
     ) {
-        serviceRegistry.register(T::class.java, instance, tag)
+        requireServiceRegistry().register(T::class.java, instance, tag)
     }
 
     protected inline fun <reified T : Any> unregisterService(tag: String? = null) {
-        serviceRegistry.unregister(T::class.java, tag)
+        requireServiceRegistry().unregister(T::class.java, tag)
     }
 
     protected fun unregisterService(instance: Any) {
-        serviceRegistry.unregister(instance)
+        requireServiceRegistry().unregister(instance)
     }
 }
