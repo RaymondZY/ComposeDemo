ad78b8c701458ea80c70819274320e46ee6924c5
zhaoyun <zhaoyun1224@gmail.com>
2026-05-03 00:26:16 +0800

    feat: add LocalKoinScope CompositionLocal

--- scaffold/android/src/main/kotlin/zhaoyun/example/composedemo/scaffold/android/ServiceRegistryCompositionLocal.kt
+++ scaffold/android/src/main/kotlin/zhaoyun/example/composedemo/scaffold/android/ServiceRegistryCompositionLocal.kt
@@ -3,12 +3,18 @@ package zhaoyun.example.composedemo.scaffold.android
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.compositionLocalOf
+import org.koin.core.scope.Scope
 import zhaoyun.example.composedemo.scaffold.core.spi.MutableServiceRegistry
 
 /**
  * CompositionLocal —— 向下传递当前 Screen 的 [MutableServiceRegistry]
  */
 val LocalServiceRegistry = compositionLocalOf<MutableServiceRegistry?> { null }
+
+/**
+ * CompositionLocal —— 向下传递当前 Screen 的 Koin [Scope]
+ */
+val LocalKoinScope = compositionLocalOf<Scope?> { null }
 
 /**
  * 向当前 Compose 子树暴露既有的服务作用域。
